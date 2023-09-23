1ee330f0e1c66258cbd818887863e5a488d42eee
0426orange <114186672+0426orange@users.noreply.github.com>
2023-09-23 16:50:17 -0600

    Add files via upload

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int usernumber = -1;
+        while(usernumber != 0){
+            Console.Write("Enter a list of numbers, type 0 when finished.");
+
+            string enter = Console.ReadLine();
+            usernumber = int.Parse(enter);
+
+
+            if (usernumber != 0){
+                
+                numbers.Add(usernumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+
+        int max = numbers[0];
+        foreach (int number in numbers){
+            if (number  > max){
+                max = number;
+            }
+        }
+        Console.WriteLine($"The largest number is:{max}");
+
+        int quantity = 0;
+        int average = 0;
+        quantity = numbers.Count;
+        average = sum/quantity;
+        Console.WriteLine($"The average is:{average}");
+
+
+
+
     }
 }
